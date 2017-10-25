4bae143ce03c4a64d592f98757e417ed50b4a978
Dario Soto <sotodario3@gmail.com>
2017-10-25 16:08:43 -0300

    Obtener Mt

---
+++ libs/obtener_mt.js
@@ -0,0 +1,43 @@
+let fs = require('fs')
+var moment  = require('moment')
+var hoy     = moment().format("YYYY-MM-DD")
+var _       = require('lodash')
+
+var getMt = (db, array) => {
+  return new Promise((resolve, reject) => {
+let sql = ''
+  array.map(e => {
+  sql += `\n SELECT count(*) count, id_producto, origen FROM sms.smsin WHERE origen = '${e[0].recipient}'
+          AND id_producto = '${e[0].product_id}' AND data_arrive BETWEEN '${moment(e[0].date).format("YYYY-MM-DD")}'
+          AND '${moment(e[0].date_notifications_r).format("YYYY-MM-DD")}'; `
+  })
+  fs.writeFile('ObtenerFechaMt.txt', sql, (err) => {
+    if(err) {
+      console.log('error: ', err)
+    } 
+  })
+    db.query(sql, (err, result) => {
+      if(err) reject(err)
+      else {
+        var data = []
+        result.map(element => {
+          if(element[0].count != 0) {
+            let a = _.find(array, { 'recipient': element[0].origen, 'product_id': element[0].id_producto });
+            console.log(array)
+            data.push(element)
+          }
+        })
+        fs.writeFile('./test/mtData.json', JSON.stringify(data), (err) => {
+        if(err) {
+          console.log('error: ', err)
+          } 
+        })
+        resolve(data)
+      }
+    })
+  })
+}
+
+module.exports = {
+  getMt
+} 
--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-var mysql = require('mysql')
-var config = require('./database/config')
-var productos = require('./libs/obtener_productos')
-var proveedor = require('./libs/proveedor_contenido')
-var actualizar = require('./libs/comprobar_actualizacion')
-var numero = require('./libs/obtener_numeros')
+var mysql       = require('mysql')
+var config      = require('./database/config')
+var productos   = require('./libs/obtener_productos')
+var proveedor   = require('./libs/proveedor_contenido')
+var actualizar  = require('./libs/comprobar_actualizacion')
+var numero      = require('./libs/obtener_numeros')
 var comprobados = require('./libs/numeros_comprobados')
+var mt          = require('./libs/obtener_mt')
 
 var db = mysql.createConnection(config)
 
 db.connect()
 
 setPromotion(db).then((doc) => {
-  // console.log(doc.length)
   db.end()
 }).catch((e) => {
   console.log(e)
@@ -24,4 +24,5 @@ async function setPromotion(db) {
   var actualizados       = await actualizar.getProducts(db, proveedores)
   var numeros            = await numero.getNums(db, actualizados)
   var numerosComprobados = await comprobados.getCount(db, numeros)
+  var mtEnviado          = await mt.getMt(db, numerosComprobados)
 }
